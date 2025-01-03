5ec03d4374108bd56383ed4a973b2b6d7274a371
nenadInforma <nenad.damjanovic@informa.com>
2025-01-03 17:20:35 +0100

    final version

--- cypress/POM/registerPage.js
+++ cypress/POM/registerPage.js
@@ -39,8 +39,157 @@ class registerPage {
     registerCompleatedFild(){
         return cy.get('.result');
     }
+
+    loginTab(){
+        return cy.get('[href="/login"]');
+    }
+
+    loginButton(){
+        return cy.get('[class="button-1 login-button"]');
+    }
+
+    logouTab(){
+        return cy.get('[href="/logout"]');
+    }
+
+    computersTabMainMenu(){
+        return cy.get('[href="/computers"]').eq(0);
+    }
+
+    giftCardTabMainMenu(){
+        return cy.get('[href="/gift-cards"]').eq(0);
+    }
+
+    computersSubGroups(){
+        return cy.get('.sub-category-item');
+    }
+
+    computersSubGroupsTitle(){
+        return cy.get('.title');
+    }
+
+    sortingButton(){
+        return cy.get('#products-orderby');
+    }
+
+    itemsPerPageButton(){
+        return cy.get('#products-pagesize');
+    }
+
+    productItems(){
+        return cy.get('.picture');
+    }
     
-    registerFlow (firstName, lastName, email, password){
+    giftCard2RecipientName(){
+        return cy.get('#giftcard_2_RecipientName')
+    }
+
+    giftCard2RecipientEmail(){
+        return cy.get('#giftcard_2_RecipientEmail')
+    }
+
+    addToWishlistButton(){
+        return cy.get('#add-to-wishlist-button-2')
+    }
+
+    whishlistTab(){
+        return cy.get('.cart-label').contains('Wishlist');
+    }
+
+    whishlistShareLink(){
+        return cy.get('[class="share-link"]');
+    }
+
+    addToCartListButton(){
+        return cy.get('[value="Add to cart"]');
+    }
+
+    addToCartButton(){
+        return cy.get('#add-to-cart-button-72');
+    }
+
+    cartTab(){
+        return cy.get('.cart-label').contains('cart');
+    }
+
+    qtyField(){
+        return cy.get('.qty-input');
+    }
+
+    itemCheckbox(){
+        return cy.get('[name="removefromcart"]');
+    }
+
+    updateCartButton(){
+        return cy.get('[name="updatecart"]');
+    }
+
+    orderSummaryField(){
+        return cy.get('.order-summary-content');
+    }
+
+    termOfServiceCheckbox(){
+        return cy.get('#termsofservice');
+    }
+
+    checkoutButton(){
+        return cy.get('#checkout');
+    }
+
+    countryField(){
+        return cy.get('#BillingNewAddress_CountryId');
+    }
+
+    stateField(){
+        return cy.get('#BillingNewAddress_StateProvinceId');
+    }
+
+    cityInputField(){
+        return cy.get('#BillingNewAddress_City');
+    }
+
+    addressInputField(){
+        return cy.get('#BillingNewAddress_Address1');
+    }
+
+    zipInputField(){
+        return cy.get('#BillingNewAddress_ZipPostalCode');
+    }
+
+    phoneInputField(){
+        return cy.get('#BillingNewAddress_PhoneNumber');
+    }
+
+    continueBillingButton(){
+        return cy.get('[onclick="Billing.save()"]');
+    }
+
+    continueShippingButton(){
+        return cy.get('[onclick="Shipping.save()"]');
+    }
+
+    continueShippingMethodButton(){
+        return cy.get('[onclick="ShippingMethod.save()"]');
+    }
+
+    continuePaymentMethodButton(){
+        return cy.get('[onclick="PaymentMethod.save()"]');
+    }
+
+    continuePaymentInfodButton(){
+        return cy.get('[onclick="PaymentInfo.save()"]');
+    }
+
+    confirmeButton(){
+        return cy.get('[value="Confirm"]');
+    }
+
+    confirmationMessage(){
+        return cy.get('.title');
+    }
+
+
+    registerComplete (firstName, lastName, email, password){
          this.registerTab().click();   
          this.genderMale().click();
          this.firstNameInputField().type(firstName);
@@ -50,6 +199,35 @@ class registerPage {
          this.passwordConfirmationInputField().type(password);
          this.registerButton().click();
     }
+
+    loginComplete (email, password){
+        this.loginTab().click();   
+        this.emailInputField().type(email);
+        this.passwordlInputField().type(password);
+        this.loginButton().click();
+   }
+
+    recipientGiftCard2 (recipientName, recipientEmail){  
+        this.giftCard2RecipientName().type(recipientName);
+        this.giftCard2RecipientEmail().type(recipientEmail);
+        this.addToWishlistButton().click();
+    }   
+
+    checkoutProcess (city, address, zip, phoneNumber ){  
+        this.countryField().select(1);
+        this.stateField().select(4);
+        this.cityInputField().type(city);
+        this.addressInputField().type(address);
+        this.zipInputField().type(zip);
+        this.phoneInputField().type(phoneNumber);
+        this.continueBillingButton().click();
+        this.continueShippingButton().click();
+        this.continueShippingMethodButton().click();
+        this.continuePaymentMethodButton().click();
+        this.continuePaymentInfodButton().click();
+        this.confirmeButton().click();
+    }   
+
 }    
 
 module.exports = new registerPage();
--- cypress/e2e/task-2.spec.cy.js
+++ cypress/e2e/task-2.spec.cy.js
@@ -5,19 +5,100 @@ const firstName = faker.person.firstName();
 const lastName = faker.person.lastName();
 const email = faker.internet.email();
 const password = faker.internet.password();
+const recipientName = faker.person.firstName();
+const recipientEmail = faker.internet.email();
+const city = faker.location.city();
+const address = faker.location.streetAddress();
+const zip = faker.location.zipCode();
+const phoneNumber = faker.phone.number();
 
 describe ('Homework Task 2', () => {
-
-    const baseUrl = Cypress.config('baseUrl');
    
-         beforeEach ('Visit Web shop site', ()=>{
-            cy.visit('/');
+         beforeEach('Visit Web shop site', ()=>{
+           cy.visit('/');
          })
 
          it('Register user', () =>{
-            registerPage.registerFlow(firstName, lastName, email, password);
+            registerPage.registerComplete(firstName, lastName, email, password);
             registerPage.registerCompleatedFild().should('contain', 'Your registration completed');
+            registerPage.logouTab().click();
          })
+
+         it('Login user', () =>{
+            registerPage.loginComplete(email, password);
+            registerPage.computersTabMainMenu().click();
+         })
+
+         it('Checking computer subgroups', () =>{
+            registerPage.computersTabMainMenu().click();
+            registerPage.computersSubGroups().should('have.length', 3);
+            registerPage.computersSubGroupsTitle().eq(3).should('contain', 'Desktops');
+            registerPage.computersSubGroupsTitle().eq(4).should('contain', 'Notebooks');
+            registerPage.computersSubGroupsTitle().eq(5).should('contain', 'Accessories');
+         })
+
+         it('Checking sorting functionality', () =>{
+            registerPage.computersTabMainMenu().click();
+            registerPage.computersSubGroupsTitle().eq(3).click();
+            registerPage.sortingButton().select(3).should('contain', 'Price: Low to High');
+         })
+
+         it('Checking sorting functionality', () =>{
+            registerPage.computersTabMainMenu().click();
+            registerPage.computersSubGroupsTitle().eq(3).click();
+            registerPage.itemsPerPageButton().select(0);
+            registerPage.itemsPerPageButton().should('contain', "4");
+         })
+
+         it('Adding items to wishlist', () =>{
+            registerPage.loginComplete(email, password);
+            registerPage.computersTabMainMenu().click();
+            registerPage.giftCardTabMainMenu().click();
+            registerPage.productItems().eq(1).click();
+            registerPage.recipientGiftCard2(recipientName, recipientEmail);
+            registerPage.whishlistTab().click();
+            registerPage.qtyField().should('not.have.value', 0);
+            registerPage.whishlistShareLink().should('be.visible').and('contain', 'http://demowebshop.tricentis.com/wishlist/');
+         })
+
+         it('Adding item to shopping cart', () =>{
+            registerPage.loginComplete(email, password);
+            registerPage.computersTabMainMenu().click();
+            registerPage.computersSubGroups().eq(0).click();
+            registerPage.addToCartListButton().eq(0).click();
+            registerPage.addToCartButton().click();
+            registerPage.cartTab().eq(0).click();
+            registerPage.qtyField().should('not.have.value', 0);
+         })
+
+         it('Removing item from shopping cart', () =>{
+            registerPage.loginComplete(email, password);
+            registerPage.computersTabMainMenu().click();
+            registerPage.computersSubGroups().eq(0).click();
+            registerPage.addToCartListButton().eq(0).click();
+            registerPage.addToCartButton().click();
+            registerPage.cartTab().eq(0).click();
+            registerPage.qtyField().should('not.have.value', 0);
+            registerPage.itemCheckbox().check();
+            registerPage.updateCartButton().click();
+            registerPage.orderSummaryField().should('contain', 'Your Shopping Cart is empty!');
+         })
+
+         it('Item checkout', () =>{
+            registerPage.loginComplete(email, password);
+            registerPage.computersTabMainMenu().click();
+            registerPage.computersSubGroups().eq(0).click();
+            registerPage.addToCartListButton().eq(0).click();
+            registerPage.addToCartButton().click();
+            registerPage.cartTab().eq(0).click();
+            registerPage.qtyField().should('not.have.value', 0);
+            registerPage.termOfServiceCheckbox().check();
+            registerPage.checkoutButton().click();
+            registerPage.checkoutProcess(city, address, zip, phoneNumber);
+            registerPage.confirmationMessage().should('contain', 'Your order has been successfully processed!');
+         })
+
+
 
          
       
